01961c5bce423bd174f152f9d80dc8cefd6464f7
Rodrigo Perez <rvicenteperez@gmail.com>
2024-07-19 13:39:04 -0400

    testing directory changes

--- src/blackjack/usecases/crear-carta.js
+++ src/blackjack/usecases/crear-carta.js
@@ -8,7 +8,7 @@ export const crearCarta = (carta) => {
   if (!carta) throw Error("la carta es un argumento obligatorio");
   // <img class="carta" src="assets/cartas/2C.png">
   const imgCarta = document.createElement("img");
-  imgCarta.src = `public/assets/cartas/${carta}.png`; //3H, JD
+  imgCarta.src = `/assets/cartas/${carta}.png`; //3H, JD
   imgCarta.classList.add("carta");
 
   return imgCarta;

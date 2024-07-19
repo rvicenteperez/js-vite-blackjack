4b723c346971f9fb52c67262580bf2c11bc9b858
Rodrigo Perez <rvicenteperez@gmail.com>
2024-07-19 11:32:26 -0400

    First commit

---
+++ src/blackjack/usecases/crear-carta.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param {String} carta
+ * @returns {HTMLImageElement} imagen de retorno
+ */
+
+export const crearCarta = (carta) => {
+  if (!carta) throw Error("la carta es un argumento obligatorio");
+  // <img class="carta" src="assets/cartas/2C.png">
+  const imgCarta = document.createElement("img");
+  imgCarta.src = `public/assets/cartas/${carta}.png`; //3H, JD
+  imgCarta.classList.add("carta");
+
+  return imgCarta;
+};
